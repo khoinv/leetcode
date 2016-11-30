d3df4b14627e0ff7a9172758bb408823b9746393
Nguyen Van Khoi <khoinvbg@gmail.com>
2016-11-30 21:16:30 +0900

    update #6

--- q6/s2.py
+++ q6/s2.py
@@ -8,36 +8,39 @@ class Solution(object):
         if numRows == 1:
             return s
         l = len(s)
+
         class Node(object):
             def __init__(self, current):
-                self.current = current
+                self._current = current
                 self.next = None
-            def getCurrent(self):
-                return self.current
+
+            def current(self):
+                return self._current
+
             def next(self):
                 return self.next
 
-        head1 = Node(0)
-        tmp1 = head1
-        head2 = Node(numRows -1)
-        tmp2 = head2
-        result = ['','']
+        head = Node(0)
+        tmp = head
+        recusive_head = Node(numRows - 1)
+        rtmp = recusive_head
+        result = ['', '']
 
-        for i in range(1, numRows -1):
-            tmp1.next = Node(i)
-            tmp1 = tmp1.next
-            tmp2.next = Node(numRows - i - 1)
-            tmp2 = tmp2.next
+        for i in range(1, numRows - 1):
+            tmp.next = Node(i)
+            tmp = tmp.next
+            rtmp.next = Node(numRows - i - 1)
+            rtmp = rtmp.next
 
             result.append('')
 
-        tmp1.next = head2
-        tmp2.next = head1
+        tmp.next = recusive_head
+        rtmp.next = head
 
-        tmp = head1
+        tmp = head
 
         for j in range(0, l):
-            result[tmp.getCurrent()] += s[j]
+            result[tmp.current()] += s[j]
             tmp = tmp.next
 
         return ''.join(result)
@@ -46,7 +49,8 @@ class Solution(object):
 def main():
     s = 'PAYPALISHIRING'
     sl = Solution()
-    sl.convert(s,3)
+    sl.convert(s, 3)
+
 
 if __name__ == '__main__':
     main()
